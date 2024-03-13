c88450fc91c408dd94dc9e6d46cc7f85069a9417
Your Name <you@example.com>
2024-03-13 18:28:47 +0530

    static block created

--- src/staticblock/Static.java
+++
@@ -1,4 +0,0 @@
-package staticblock;
-
-public class Static {
-}
---
+++ src/staticblock/StaticBlock.java
@@ -0,0 +1,24 @@
+package staticblock;
+
+public class StaticBlock {
+
+
+        static int a = 45;
+        static int b;
+  // this block will only run when the first object is created and the class loaded and it only run one time only
+        static {
+            System.out.println("static block created.");
+            b = a*5;
+        }
+
+        public static void main(String[] args) {
+           StaticBlock obj = new StaticBlock();
+            System.out.println(StaticBlock.a+" "+StaticBlock.b);
+            StaticBlock.b = StaticBlock.b+3;
+            StaticBlock obj2 = new StaticBlock();
+            System.out.println(StaticBlock.a + " "+StaticBlock.b);
+
+
+        }
+    }
+
